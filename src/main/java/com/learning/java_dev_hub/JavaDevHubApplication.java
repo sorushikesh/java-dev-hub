bc2b7d995dc629d556ba7ee13b606d8b20731118
sorushikesh <sorushikesh07@gmail.com>
2025-06-08 20:08:08 +0530

    Initial commit

---
+++ src/main/java/com/learning/java_dev_hub/JavaDevHubApplication.java
@@ -0,0 +1,13 @@
+package com.learning.java_dev_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevHubApplication.class, args);
+	}
+
+}
