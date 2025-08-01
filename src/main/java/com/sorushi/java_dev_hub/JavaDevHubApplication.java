6b680f69368419aa0c09d643d85cf462a59d8a16
Rushikesh <sorushikesh07@gmail.com>
2025-08-01 11:27:50 +0530

    Setup

---
+++ src/main/java/com/sorushi/java_dev_hub/JavaDevHubApplication.java
@@ -0,0 +1,12 @@
+package com.sorushi.java_dev_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevHubApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaDevHubApplication.class, args);
+  }
+}
---
+++ src/test/java/com/sorushi/java_dev_hub/JavaDevHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorushi.java_dev_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
