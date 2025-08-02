04039147a6c7765bed72cb6a0f5e38f4bb04b1a9
Rushikesh <sorushikesh07@gmail.com>
2025-08-02 09:29:54 +0530

    Reformat

--- src/test/java/com/sorushi/java_dev_hub/JavaDevHubApplicationTests.java
+++ src/test/java/com/sorushi/java_dev_hub/JavaDevHubApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaDevHubApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
