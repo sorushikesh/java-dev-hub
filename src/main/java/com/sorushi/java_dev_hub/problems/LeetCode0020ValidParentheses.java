9ca3fbfe33845ac197ebab27017372d6ce6fe42b
Rushikesh <sorushikesh07@gmail.com>
2025-08-02 18:55:06 +0530

    Leetcode

---
+++ src/main/java/com/sorushi/java_dev_hub/problems/LeetCode0020ValidParentheses.java
@@ -0,0 +1,27 @@
+//package com.sorushi.java_dev_hub.problems;
+//
+//import lombok.extern.slf4j.Slf4j;
+//
+///**
+// * Given a string s containing just the characters '(', ')', '{', '}',
+// * '[' and ']', determine if the input string is valid.
+// */
+//
+//@Slf4j
+//public class LeetCode0020ValidParentheses {
+//
+//  public static void main(String[] args){
+//
+//    String input = "()[]{}";
+//    isValid(input);
+//  }
+//
+//  public static boolean isValid(String s) {
+//
+//    String tempVar;
+//
+//    for (int i = 0; i < s.length(); i++) {
+//
+//    }
+//  }
+//}
